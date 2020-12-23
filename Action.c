6fa0faf27cd88eb6bcc9276739ae500204ba0e33
unknown <unknown>
2020-12-23 07:16:58 +0100

    new reverterd changes

--- Action.c
+++ Action.c
@@ -2,17 +2,5 @@ Action()
 {
 	//newly created
 	
-
-	web_custom_request("web_custom_request",
-		"URL=https://www.example.com",
-		"Method=GET",
-		"TargetFrame=",
-		"Resource=0",
-		"Referer=",
-		"Body=",
-		LAST);
-
-	
-	
 	return 0;
 }
